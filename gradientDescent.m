e257bcde145c83f02d145ed0a325feb070e6c10f
Maria Baron <166757495+MariaTBaronV@users.noreply.github.com>
2024-05-16 16:39:43 -0500

    gradientDescent.m

--- gradientDescent.m
+++ gradientDescent.m
@@ -7,7 +7,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
-for iter = 1:num_iters
+
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
@@ -15,12 +15,14 @@ for iter = 1:num_iters
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
-    %
 
-
-
-
-
+for iter = 1:num_iters
+    k = 1:m;
+    t1 = sum((theta(1) + theta(2) .* X(k,2)) - y(k)); % Un-Vectorized
+    t2 = sum(((theta(1) + theta(2) .* X(k,2)) - y(k)) .* X(k,2)); % Un-Vectorized
+    
+    theta(1) = theta(1) - (alpha/m) * (t1);
+    theta(2) = theta(2) - (alpha/m) * (t2);
 
 
     % ============================================================
